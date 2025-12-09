8f24fda7272bf4f521b2169f3ced1a537fd8fde4
Jonáš Bučina <99987632+silenyJonas@users.noreply.github.com>
2025-12-09 23:41:32 +0100

    build 8

--- after_build.bat
+++ after_build.bat
@@ -1,21 +1,53 @@
 @echo off
-set "SOURCE_DIR=dist\portfolium_jb\browser"
+
+:: Jméno repozitáře pro --base-href
+set "REPO_NAME=portfolium"
+set "BUILD_CONFIG=production"
+
+:: Dulezite: Jméno VÝSTUPNÍ SLOŽKY generované Angularem (dist\[nazev_projektu]\browser)
+set "OUTPUT_FOLDER=portfolium_jb" 
+
+set "SOURCE_DIR=dist\%OUTPUT_FOLDER%\browser"
 set "TARGET_DIR=docs"
 
-echo --- Pripravuji nasazeni pro GitHub Pages ---
+echo --- Spoustim build a nasazeni pro GitHub Pages ---
 
-:: Krok 1: Smazani obsahu cilove slozky (docs)
-echo Cistim cilovou slozku: %TARGET_DIR%
+:: -----------------------------------------------------
+:: Krok 1: Spusteni produkcniho buildu Angular
+:: -----------------------------------------------------
+echo.
+echo Krok 1: Spoustim Angular build s --base-href /%REPO_NAME%/
+call ng build --base-href /%REPO_NAME%/ --configuration %BUILD_CONFIG%
+
+if errorlevel 1 (
+    echo.
+    echo CHYBA: Angular build selhal. Zkontrolujte vypis chyb.
+    pause
+    exit /b 1
+)
+echo.
+echo Angular build dokoncen.
+
+:: -----------------------------------------------------
+:: Krok 2: Priprava a cisteni cilove slozky (docs)
+:: -----------------------------------------------------
+echo.
+echo Krok 2: Cistim a pripravuji cilovou slozku: %TARGET_DIR%
 if exist "%TARGET_DIR%" (
     rmdir /s /q "%TARGET_DIR%"
 )
-
-:: Krok 2: Vytvoreni prazdne cilove slozky
 mkdir "%TARGET_DIR%"
 
+:: -----------------------------------------------------
 :: Krok 3: Kopirovani souboru z buildu do cilove slozky
-echo Kopiruji soubory z %SOURCE_DIR% do %TARGET_DIR%
+:: -----------------------------------------------------
+echo.
+echo Krok 3: Kopiruji soubory z %SOURCE_DIR% do %TARGET_DIR%
 xcopy "%SOURCE_DIR%" "%TARGET_DIR%" /e /i /h /y
 
-echo --- Nasazeni dokončeno! ---
+echo.
+echo ================================================
+echo Nasazeni do slozky %TARGET_DIR% dokončeno!
+echo Nezapomente COMMITNOUT a PUSHNOUT zmeny na GitHub.
+echo ================================================
 pause
